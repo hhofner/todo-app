99067c1e834bd4088d421b5734c16584553279c8
Hans Hofner <hhofner@gmail.com>
2023-12-21 14:49:27 +0900

    feat: add basic structure html file

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'))
+
+// The route handler is now simplified as the static file will be served automatically
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + '/public/index.html'); // Optional if 'index.html' is your file
 })
 
 app.listen(port, () => {
